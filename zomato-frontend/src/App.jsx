adabb67a330a90eec1ca38f23c8983ce6b1d5c54
Akash Ghosh <111114759+AXC-Git@users.noreply.github.com>
2025-02-24 00:59:40 +0000

    css fixes

--- zomato-frontend/src/App.jsx
+++ zomato-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from "./pages/Home";
-import '../App.css';
+import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
